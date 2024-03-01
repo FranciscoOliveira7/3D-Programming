450bd4bf52708cbc54e44e495776af430028cd65
Francisco Oliveira <64752398+Sincopse@users.noreply.github.com>
2024-03-01 09:37:39 +0000

    gregreg

--- space_game/collision_box.h
+++ space_game/collision_box.h
@@ -7,20 +7,30 @@
 namespace my_game {
 
 	class CollisionBox : public Rectangle {
+	 public:
+		CollisionBox(float x, float y, float w, float h) : Rectangle(x + w / 2, y + h / 2, w, h), hit_{ false } {};
+		bool ComputeCollision(CollisionBox&);
+
+	 private:
 		bool hit_ = false;
-
-	public:
-		CollisionBox(float x, float y, float w, float h) : Rectangle(x + w / 2, y + h / 2, w, h) {
-			std::cout << "Collision box created";
-		}
-
-		void ComputeCollision(CollisionBox other) {
-			float x_min = std::min<float>(center_x_ - width_ / 2.0f, other.center_x_ - other.width_ / 2.0f);
-			float x_max = std::max<float>(center_x_ + width_ / 2.0f, other.center_x_ + other.width_ / 2.0f);
-			float y_min = std::min<float>(center_y_ - height_ / 2.0f, other.center_y_ - other.height_ / 2.0f);
-			float y_max = std::max<float>(center_y_ + height_ / 2.0f, other.center_y_ + other.height_ / 2.0f);
-
-			//if (((x_min - x_max) < (width_ - other.width_)) || ((x_min - x_max) < (width_ - other.width_)))
-		}
 	};
+
+	bool CollisionBox::ComputeCollision(CollisionBox& other) {
+		float min_x = std::min<float>(center_x_ - width_ / 2.0f, other.center_x_ - other.width_ / 2.0f);
+		float max_x = std::max<float>(center_x_ + width_ / 2.0f, other.center_x_ + other.width_ / 2.0f);
+		float min_y = std::min<float>(center_y_ - height_ / 2.0f, other.center_y_ - other.height_ / 2.0f);
+		float max_y = std::max<float>(center_y_ + height_ / 2.0f, other.center_y_ + other.height_ / 2.0f);
+
+		if (((min_x - max_x) < (width_ - other.width_)) || ((min_y - max_y) < (height_ - other.height_))) {
+			hit_ = true;
+			other.hit_ = true;
+
+			std::cout << "Collision Detected!";
+		}
+		else {
+			hit_ |= false;
+		}
+
+		return hit_;
+	}
 }
