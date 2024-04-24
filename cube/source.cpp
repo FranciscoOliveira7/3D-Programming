39d94042728856cbe86e3076b80095fd9749f135
Francisco Oliveira <64752398+Sincopse@users.noreply.github.com>
2024-04-24 17:08:38 +0100

    njnjnjn

--- cube/source.cpp
+++ cube/source.cpp
@@ -37,12 +37,14 @@ int main(void)
     glfwMakeContextCurrent(window);
 
 	std::vector<vec3> cube = LoadModel();
-	float zoom = 10.0f;
+	float zoom = 10.0f, rotation = 0.0f;
+
+	mat4 projection = perspective(radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
 
     while (!glfwWindowShouldClose(window))
 	{
-		mat4 projection = perspective(radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
 		mat4 model = mat4(1.0f);
+		model = rotate(model, rotation += 0.0001, vec3(1, .5, 0));
 		mat4 view = lookAt(
 			vec3(0, 0, zoom),
 			vec3(0, 0, -1),
@@ -63,14 +65,22 @@ int main(void)
 
 void Draw(mat4 mvp, std::vector<vec3> model) {
 
+	vec3 colors[] = {
+		vec3(1, 0, 0), // red
+		vec3(0, 0, 1), // blue
+		vec3(1, 1, 0), // yellow
+		vec3(0, 1, 0), // green
+		vec3(0, 1, 1), // cyan
+		vec3(1, 0, 1)  // magenta
+	};
+
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glBegin(GL_QUADS);
-
-	glColor3f(1.0f, 1.0f, 1.0f);
 	
 	for (int i = 0; i < 6 * 4; i++) {
 
+		glColor3f(colors[i / 4].r, colors[i / 4].g, colors[i / 4].b);
 		vec4 vertex = vec4(model[i].x, model[i].y, model[i].z, 1.0f);
 		vec4 transformed_vertex = mvp * vertex;
 		vec4 normalized_vertex = transformed_vertex / transformed_vertex.w;
