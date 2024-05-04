d9fecb9f1b6dd0487ddac817d6febbc84c5c2598
Francisco Oliveira <64752398+Sincopse@users.noreply.github.com>
2024-05-04 17:00:33 +0100

    glow exercise

--- mouse_glow/shader_util.cpp
+++ mouse_glow/shader_util.cpp
@@ -8,6 +8,8 @@
 
 #include "shader_util.h"
 
+void DestroyShaders(ShaderInfo*);
+
 static const GLchar* ReadShader(const char* filename) {
 	// Abre o ficheiro 'filename' em binário, e coloca-se na última posição do ficheiro.
 	std::ifstream file(filename, std::ifstream::ate, std::ifstream::binary);
@@ -33,4 +35,64 @@ static const GLchar* ReadShader(const char* filename) {
 	else std::cerr << "Error opening the file \"" << filename << "\"" << std::endl;
 
 	return nullptr;
+}
+
+GLuint LoadShaders(ShaderInfo* shaders) {
+	if (shaders == nullptr) return 0;
+
+	GLuint program = glCreateProgram();
+
+	for (GLuint i = 0; i < shaders[i].type != GL_NONE; i++) {
+		// Criar um objeto shader
+		shaders[i].shader = glCreateShader(shaders[i].type);
+
+		const GLchar* source = ReadShader(shaders[i].filename);
+
+		// Destroi os shaders caso não consiga ler o código
+		if (source == NULL) {
+			DestroyShaders(shaders);
+			return 0;
+		}
+
+		// Carrega o código do shader
+		glShaderSource(shaders[i].shader, 1, &source, NULL);
+		delete[] source;
+
+		// Compila o shader
+		glCompileShader(shaders[i].shader);
+
+		// Verifica o estado da compilação
+		GLint compiled;
+		glGetShaderiv(shaders[i].shader, GL_COMPILE_STATUS, &compiled);
+		// Em caso de erro na compilação
+		if (!compiled) {
+			DestroyShaders(shaders);
+			return 0;
+		}
+
+		// Anexa o shader ao programa
+		glAttachShader(program, shaders[i].shader);
+	}
+
+	// Linka o programa
+	glLinkProgram(program);
+
+	// Verifica o estado do processo de linkagem
+	GLint linked;
+	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+	// Caso dê erro na linkagem
+	if (!linked) {
+		DestroyShaders(shaders);
+		return 0;
+	}
+
+	return program;
+}
+
+void DestroyShaders(ShaderInfo* shaders) {
+	for (int j = 0; shaders[j].type != GL_NONE; j++) {
+		// Caso tenha um shader válido (i.e., != 0)
+		if (shaders[j].shader != 0) glDeleteShader(shaders[j].shader);
+		shaders[j].shader = 0;
+	}
 }
--- mouse_glow/source.cpp
+++ mouse_glow/source.cpp
@@ -11,18 +11,38 @@ using namespace std;
 #define GLFW_USE_DWM_SWAP_INTERVAL
 #include <GLFW\glfw3.h>
 
+#include <glm/glm.hpp> // vec3, vec4, ivec4, mat4, ...
 #include "shader_util.h"
+
+#define WIDTH 640
+#define HEIGHT 480
+
+void print_error(int error, const char* description);
+void init(void);
+void display(void);
+
+float mousePos[2] = { 1., 0. };
+GLuint program = 0;
+
+void cursorCallBack(GLFWwindow* window, double x, double y) {
+
+    mousePos[0] = x;
+    mousePos[1] = HEIGHT - y;
+    cout << "Cursor x: " << mousePos[0] << "y: "  << mousePos[1] << endl;
+}
 
 int main(void)
 {
     GLFWwindow* window;
+
+    glfwSetErrorCallback(print_error);
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Mouse Glow", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -32,19 +52,60 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    // Inicia o gestor de extensões GLEW
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    init();
+
+    glfwSetCursorPosCallback(window, cursorCallBack);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        GLint mousePosId = glGetProgramResourceLocation(program, GL_UNIFORM, "mousePos");
+        glProgramUniform2fv(program, mousePosId, 1, mousePos);
+
+        display();
+
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void print_error(int error, const char* description) {
+    cout << description << endl;
+}
+
+void init(void) {
+    ShaderInfo shaders[] = {
+        { GL_VERTEX_SHADER, "triangles.vert" },
+        { GL_FRAGMENT_SHADER, "glow.frag" },
+        { GL_NONE, NULL }
+    };
+
+    program = LoadShaders(shaders);
+    if (!program) exit(EXIT_FAILURE);
+    glUseProgram(program);
+
+    float screenSize = sqrtf(HEIGHT * HEIGHT + WIDTH * WIDTH);
+
+    GLint screenSizeId = glGetProgramResourceLocation(program, GL_UNIFORM, "screen");
+    glProgramUniform1f(program, screenSizeId, screenSize);
+}
+
+void display(void) {
+    static const float black[] = {
+        0.0f, 0.0f, 0.0f, 0.0f
+    };
+    
+	glClearBufferfv(GL_COLOR, 0, black);
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 }
